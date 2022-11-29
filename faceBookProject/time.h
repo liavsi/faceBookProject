20c566051c3b956e89a919dab0d4427e79a61c71
liavsi <liavsi@mta.ac.il>
2022-11-29 12:26:57 +0200

    add include to ctime

--- faceBookProject/time.h
+++ faceBookProject/time.h
@@ -1,12 +1,15 @@
 #ifndef TIME
 #define TIME
 #include <iostream>
+#include <ctime>
 using namespace std;
+
 
 class Time				
 {
 public:
 	Time();
+	Time(const char* timeText);
 	void showTime();
 
 private:
